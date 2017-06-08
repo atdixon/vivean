635a4ef81954f1544271962c678db2a7ad6fc607
Aaron Dixon <atdixon@gmail.com>
2017-06-08 10:57:13 -0500

    more

--- vroom/src/main/java/com/github/atdixon/vroom/VMap.java
+++ vroom/src/main/java/com/github/atdixon/vroom/VMap.java
@@ -1,11 +1,11 @@
 package com.github.atdixon.vroom;
 
 import com.github.atdixon.vroom.coercion.CanToMap;
+import org.pcollections.HashTreePMap;
 import org.pcollections.PMap;
 
 import javax.annotation.Nonnull;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -24,77 +24,59 @@ public final class VMap implements CanToMap<String, Object> {
 
     // -- state --
 
-    private final Map<String, ?> map;
+    private final PMap<String, Object> pmap;
 
     private VMap(Map<String, ?> map) {
-        this.map = new HashMap<>(map);
+        this.pmap = map instanceof PMap ? (PMap) map : HashTreePMap.from(map);
     }
 
     // -- producers --
 
-    public VMap assoc(String key, VMap val) {
-        return assoc(key, val.toMap());
-    }
-
     @SuppressWarnings("unchecked")
     public VMap assoc(String key, Object val) {
         final Object useVal = Shrink.shrinkToNull(val);
-        if (useVal == null) {
-            if (map.get(key) == null) {
-                return this;
-            } else {
-                return new VMap(V.one(map, PMap.class).get().minus(key));
-            }
-        } else {
-            if (useVal.equals(map.get(key))) {
-                return this;
-            } else {
-                return new VMap(V.one(map, PMap.class).get().plus(key, useVal));
-            }
-        }
+        return useVal == null
+            ? new VMap(pmap.minus(key))
+            : new VMap(pmap.plus(key, useVal));
     }
 
     @SuppressWarnings("unchecked")
     public VMap without(String key) {
-        return new VMap(V.one(map, PMap.class).get().minus(key));
+        return new VMap(pmap.minus(key));
     }
 
     @Override @SuppressWarnings("unchecked")
     public Map<String, Object> toMap() {
-        return Collections.unmodifiableMap(map);
-    }
-
-    public Map<String, Object> toShrunkMap() {
-        return Collections.unmodifiableMap(Shrink.shrink(map));
+        return pmap; // unmodifiable.
     }
 
     // -- core reads --
 
     @Nonnull
     public <T> Optional<T> one(String key, Class<T> as) {
-        return V.one(V.get(map, key), as);
+        return V.one(V.get(pmap, key), as);
     }
 
     @Nonnull
     public <T> Optional<T> one(String key, TypeSupplier<T> as) {
-        return V.one(V.get(map, key), as);
+        return V.one(V.get(pmap, key), as);
     }
 
     @Nonnull
     public <T> List<T> many(String key, Class<T> as) {
-        return V.many(V.get(map, key), as);
+        return V.many(V.get(pmap, key), as);
     }
 
     @Nonnull
     public <T> List<T> many(String key, TypeSupplier<T> as) {
-        return V.many(V.get(map, key), as);
+        return V.many(V.get(pmap, key), as);
     }
 
     // -- nice --
 
     @Override
     public String toString() {
-        return map.toString();
+        return pmap.toString();
     }
 
 }
